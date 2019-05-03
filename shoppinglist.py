81919e7ff1d6d740f496c4f981314dc6cb5a73b4
rapht0r <45973016+rapht0r@users.noreply.github.com>
2019-05-03 18:41:56 +0000

    Update shoppinglist.py

--- shoppinglist.py
+++ shoppinglist.py
@@ -10,20 +10,20 @@ import datetime
 import socket
 
 
-class ShoppingList:
+class TodoList:
     def __init__(self, config):
         self.config = config
         self.wanted_intents = []  # For reacting only with wanted intents
-        self.shoppinglist_path = ".shoppinglist"
-        io.open(self.shoppinglist_path, 'a').close()  # Create file, if not available
-        self.shoppinglist = self.read_shoppinglist()
+        self.todolist_path = ".todolist"
+        io.open(self.todolist_path, 'a').close()  # Create file, if not available
+        self.todolist = self.read_todolist()
 
     def add_item(self, intentmessage):
         item_list = [item.value for item in intentmessage.slots.item.all()]
-        dublicate_items = [item for item in item_list if item in self.shoppinglist]
-        added_items = [item for item in item_list if item not in self.shoppinglist]
+        dublicate_items = [item for item in item_list if item in self.todolist]
+        added_items = [item for item in item_list if item not in self.todolist]
         for item in added_items:
-            self.shoppinglist.append(item)
+            self.todolist.append(item)
 
         response = ""
         if added_items:
@@ -35,7 +35,7 @@ class ShoppingList:
                 items_str += "{} ".format(added_items[-1])
                 word_pl_sg = "wurde"
             first_str = items_str + random.choice(["{} hinzugefügt".format(word_pl_sg),
-                                                   "{} auf die Einkaufsliste geschrieben".format(word_pl_sg)])
+                                                   "{} auf die Todo-liste geschrieben".format(word_pl_sg)])
             if not dublicate_items:
                 first_str += "."
             else:
@@ -52,14 +52,14 @@ class ShoppingList:
             second_str = items_str + random.choice(["{} schon auf der Liste.".format(word_pl_sg),
                                                     "{} auf der Liste schon vorhanden.".format(word_pl_sg)])
             response += second_str
-        self.save_shoppinglist()
+        self.save_todolist()
         return response
 
     def remove_item(self, intentmessage):
         item_list = [item.value for item in intentmessage.slots.item.all()]
-        notlist_items = [item for item in item_list if item not in self.shoppinglist]
-        removed_items = [item for item in item_list if item in self.shoppinglist]
-        self.shoppinglist = [item for item in self.shoppinglist if item not in removed_items]
+        notlist_items = [item for item in item_list if item not in self.todolist]
+        removed_items = [item for item in item_list if item in self.todolist]
+        self.todolist = [item for item in self.todolist if item not in removed_items]
 
         response = ""
         if removed_items:
@@ -71,7 +71,7 @@ class ShoppingList:
                 items_str += "{} ".format(removed_items[-1])
                 word_pl_sg = "wurde"
             first_str = items_str + random.choice(["{} entfernt".format(word_pl_sg),
-                                                   "{} von der Einkaufsliste entfernt".format(word_pl_sg)])
+                                                   "{} von der Todo-liste entfernt".format(word_pl_sg)])
             if not notlist_items:
                 first_str += "."
             else:
@@ -86,53 +86,53 @@ class ShoppingList:
                 items_str += "{} ".format(notlist_items[-1])
                 word_pl_sg = "ist"
             second_str = items_str + random.choice(["{} nicht auf der Liste.".format(word_pl_sg),
-                                                    "{} auf der Einkaufsliste nicht vorhanden.".format(word_pl_sg)])
+                                                    "{} auf der Todo-liste nicht vorhanden.".format(word_pl_sg)])
             response += second_str
-        self.save_shoppinglist()
+        self.save_todolist()
         return response
 
     def is_item(self, intentmessage):
         item = intentmessage.slots.item.first().value
-        if item in self.shoppinglist:
-            response = "Ja, {item} steht auf der Einkaufsliste.".format(item=str(item))
+        if item in self.todolist:
+            response = "Ja, {item} steht auf der Todo-liste.".format(item=str(item))
         else:
-            response = "Nein, {item} ist nicht auf der Einkaufsliste.".format(item=str(item))
+            response = "Nein, {item} ist nicht auf der Todo-liste.".format(item=str(item))
         return response
 
     def try_clear(self):
-        if len(self.shoppinglist) > 1:
-            response = "Die Einkaufsliste enthält noch {num} Elemente." \
-                       " Bist du dir sicher?".format(num=len(self.shoppinglist))
-        elif len(self.shoppinglist) == 1:
-            response = "Die Einkaufsliste enthält noch ein Element. Bist du dir sicher?"
+        if len(self.todolist) > 1:
+            response = "Die Todo-liste enthält noch {num} Elemente." \
+                       " Bist du dir sicher?".format(num=len(self.todolist))
+        elif len(self.todolist) == 1:
+            response = "Die Todo-liste enthält noch ein Element. Bist du dir sicher?"
         else:
-            response = "empty"  # Error: Shoppinglist is already empty - no dialogue start
+            response = "empty"  # Error: Todolist is already empty - no dialogue start
         return response
 
     def clear_confirmed(self, intentmessage):
         if intentmessage.slots.answer.first().value == "yes":
-            self.shoppinglist = []
-            self.save_shoppinglist()
-            return "Die Einkaufsliste wurde geleert."
+            self.todolist = []
+            self.save_todolist()
+            return "Die Todo-liste wurde geleert."
         else:
-            return "Die Einkaufsliste wurde nicht geleert."
+            return "Die Todo-liste wurde nicht geleert."
 
     def show(self):
-        if len(self.shoppinglist) > 1:
-            shoppinglist_str = ""
-            for item in self.shoppinglist[:-1]:
-                shoppinglist_str = shoppinglist_str + str(item) + ", "
-            response = "Die Einkaufsliste enthält {items}und {last}.".format(
-                items=shoppinglist_str, last=self.shoppinglist[-1])
-        elif len(self.shoppinglist) == 1:
-            response = "Die Einkaufsliste enthält nur {item}.".format(item=self.shoppinglist[0])
-        else:  # If shoppinglist is empty
-            response = "Die Einkaufsliste ist leer."
+        if len(self.todolist) > 1:
+            todolist_str = ""
+            for item in self.todolist[:-1]:
+                todolist_str = todolist_str + str(item) + ", "
+            response = "Die Todo-liste enthält {items}und {last}.".format(
+                items=todolist_str, last=self.todolist[-1])
+        elif len(self.todolist) == 1:
+            response = "Die Todo-liste enthält nur {item}.".format(item=self.todolist[0])
+        else:  # If todolist is empty
+            response = "Die Todo-liste ist leer."
         return response
 
     def send(self):
-        if len(self.shoppinglist) == 0:
-            return "Die Einkaufsliste ist leer. Eine Email ist daher überflüssig."
+        if len(self.todolist) == 0:
+            return "Die Todo-liste ist leer. Eine Email ist daher überflüssig."
 
         try:
             email_dict = literal_eval(self.config['secret']['email_data'])
@@ -159,17 +159,17 @@ class ShoppingList:
         now = datetime.datetime.now()
         now_date = now.strftime("%d.%m.%Y")
         now_time = now.strftime("%H:%M Uhr")
-        msg['Subject'] = "Deine Einkaufsliste vom {date}".format(date=now_date)
+        msg['Subject'] = "Deine Todo-liste vom {date}".format(date=now_date)
 
-        emailtext = "Du hast eine Email mit deiner <b>Einkaufsliste</b> bekommen, weil du es "
+        emailtext = "Du hast eine Email mit deiner <b>Todo-liste</b> bekommen, weil du es "
         emailtext += "am {date} um {time} so wolltest.</br></br>".format(date=now_date, time=now_time)
         emailtext += "Hier ist die Liste:<ul>"
-        for item in self.shoppinglist:
+        for item in self.todolist:
             emailtext += "<li>{item}</li>".format(item=item)
         emailtext += '</ul></br></br></br>Diese Mail wurde automatisch generiert von der '
-        emailtext += '<a href="https://console.snips.ai/store/de/skill_Va52B5v45GB">App</a> "Einkaufsliste" '
+        emailtext += '<a href="https://console.snips.ai/store/de/skill_Va52B5v45GB">App</a> "Todo-liste" '
         emailtext += 'aus dem Snips App Store. Der Quellcode ist frei und kann '
-        emailtext += '<a href="https://github.com/MrJohnZoidberg/Snips-Einkaufsliste">hier</a> eingesehen werden. '
+        emailtext += '<a href="https://github.com/MrJohnZoidberg/Snips-Todo-liste">hier</a> eingesehen werden. '
         emailtext += 'Die Anmeldedaten, die für den Versand benötigt werden, sind lokal im System, auf dem Snips '
         emailtext += 'läuft, gespeichert.'
 
@@ -190,7 +190,7 @@ class ShoppingList:
             server.login(email_dict['from'], email_dict['password'])
             text = msg.as_string()
             server.sendmail(email_dict['from'], email_dict['to'], text)
-            response = "Die Einkaufsliste wurde als Email versendet."
+            response = "Die Todo-liste wurde als Email versendet."
         except smtplib.SMTPAuthenticationError:
             response = "Die Email konnte nicht versendet werden, weil die Anmeldedaten ungültig sind."
         except smtplib.SMTPRecipientsRefused:
@@ -200,14 +200,14 @@ class ShoppingList:
             server.quit()
         return response
 
-    def read_shoppinglist(self):
+    def read_todolist(self):
         try:
-            with io.open(self.shoppinglist_path, 'rb') as f:
+            with io.open(self.todolist_path, 'rb') as f:
                 itemlist = pickle.load(f)
             return itemlist
         except EOFError:  # if no list in file
             return []
 
-    def save_shoppinglist(self):
-        with io.open(self.shoppinglist_path, "wb") as f:
-            pickle.dump(self.shoppinglist, f)
+    def save_todolist(self):
+        with io.open(self.todolist_path, "wb") as f:
+            pickle.dump(self.todolist, f)
