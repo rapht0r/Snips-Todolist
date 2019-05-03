a2b7e2ec3211752acb527b7f38680d5cc4bc1778
rapht0r <45973016+rapht0r@users.noreply.github.com>
2019-05-03 18:34:11 +0000

    Update and rename action-ShoppingList.py to action-todolist.py

--- action-todolist.py
+++ action-todolist.py
@@ -5,10 +5,10 @@ import configparser
 from hermes_python.hermes import Hermes
 import hermes_python.ontology.dialogue as dialogue
 import io
-from shoppinglist import ShoppingList
+from todolist import TodoList
 
 
-USERNAME_INTENTS = "domi"
+USERNAME_INTENTS = "raph"
 
 
 def user_intent(intentname):
@@ -29,55 +29,55 @@ def read_configuration_file(configuration_file):
 def intent_callback(hermes, intent_message):
     # conf = read_configuration_file(CONFIG_INI)
     intentname = intent_message.intent.intent_name
-    if intentname == user_intent("addShoppingListItem"):
-        result_sentence = shoppinglist.add_item(intent_message)
+    if intentname == user_intent("addTodoListItem"):
+        result_sentence = todolist.add_item(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("removeShoppingListItem"):
-        result_sentence = shoppinglist.remove_item(intent_message)
+    elif intentname == user_intent("removeTodoListItem"):
+        result_sentence = todolist.remove_item(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("isItemOnShoppingList"):
-        result_sentence = shoppinglist.is_item(intent_message)
+    elif intentname == user_intent("isItemOnTodoList"):
+        result_sentence = todolist.is_item(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("clearShoppingList"):
-        result_sentence = shoppinglist.try_clear()
+    elif intentname == user_intent("clearTodoList"):
+        result_sentence = todolist.try_clear()
         if result_sentence == "empty":
-            result_sentence = "Die Einkaufsliste ist schon leer."
+            result_sentence = "Die Todo-Liste ist schon leer."
             hermes.publish_end_session(intent_message.session_id, result_sentence)
         else:
-            shoppinglist.wanted_intents = [user_intent("confirmShoppingList")]
-            configure_message = dialogue.DialogueConfiguration().enable_intent(user_intent("confirmShoppingList"))
+            todolist.wanted_intents = [user_intent("confirmTodoList")]
+            configure_message = dialogue.DialogueConfiguration().enable_intent(user_intent("confirmTodoList"))
             hermes.configure_dialogue(configure_message)
             hermes.publish_continue_session(intent_message.session_id, result_sentence,
-                                            shoppinglist.wanted_intents)
+                                            todolist.wanted_intents)
         
-    elif intentname == user_intent("confirmShoppingList"):
-        shoppinglist.wanted_intents = []
-        result_sentence = shoppinglist.clear_confirmed(intent_message)
+    elif intentname == user_intent("confirmTodoList"):
+        todolist.wanted_intents = []
+        result_sentence = todolist.clear_confirmed(intent_message)
         hermes.publish_end_session(intent_message.session_id, result_sentence)
     
-    elif intentname == user_intent("showShoppingList"):
-        result_sentence = shoppinglist.show()
+    elif intentname == user_intent("showTodoList"):
+        result_sentence = todolist.show()
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
-    elif intentname == user_intent("sendShoppingList"):
-        result_sentence = shoppinglist.send()
+    elif intentname == user_intent("sendTodoList"):
+        result_sentence = todolist.send()
         hermes.publish_end_session(intent_message.session_id, result_sentence)
 
 
 def intent_not_recognized_callback(hermes, intent_message):
-    configure_message = dialogue.DialogueConfiguration().disable_intent(user_intent("confirmShoppingList"))
+    configure_message = dialogue.DialogueConfiguration().disable_intent(user_intent("confirmTodoList"))
     hermes.configure_dialogue(configure_message)
-    shoppinglist.wanted_intents = []
+    todolist.wanted_intents = []
     hermes.publish_end_session({'sessionId': intent_message.session_id,
-                                'text': "Die Einkaufsliste wurde nicht gelöscht."})
+                                'text': "Die Todo-Liste wurde nicht gelöscht."})
 
 
 if __name__ == "__main__":
     config = read_configuration_file("config.ini")
-    shoppinglist = ShoppingList(config)
+    todolist = TodoList(config)
     with Hermes("localhost:1883") as h:
         h.subscribe_intents(intent_callback)
         h.subscribe_intent_not_recognized(intent_not_recognized_callback)
